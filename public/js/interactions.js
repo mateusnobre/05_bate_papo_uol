643a88de1ba33b15d2e8016c921aa19c4acc5b1b
mateusnobre <mateusnobrests@gmail.com>
2021-04-22 02:42:41 -0300

    user login and update working

--- public/js/interactions.js
+++ public/js/interactions.js
@@ -1,13 +1,36 @@
+function processError(error) {
+    console.log("Status Code: " + error.response.status); // Ex: 404
+    console.log("Error Message: " + error.response.data); // Ex: Not Found
+}
 
 let username = prompt("Como você quer ser chamado?");
+
+logUserStatus = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants', {name: username})
+logUserStatus.catch(processError);
+
+setInterval(updateUserStatus, 5000);
+setInterval(getMessages, 500);
+function updateUserStatus(){
+    console.log(username);
+    status = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status', {name: username});
+}
+function getMessages(){};
+function sendMessage(username, message, dest = 'Todos', type = 'message'){
+    let messageJSON = {
+        from: username,
+        to: dest,
+        text: message,
+        type: type
+    }
+    message_status = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages', messageJSON);
+};
 let currentTime = axios.get('http://worldtimeapi.org/api/timezone/America/Sao_Paulo');
 let chat = document.getElementsByClassName('chat')[0];
+let sendButton = document.getElementsByClassName('send_icon')[0];
 
-console.log(currentTime);
 time = currentTime.then(processDateResponse); 
-console.log(time);
 currentTime.catch(processError);
-    
+
 function processDateResponse(time) { 
     time = time.data.datetime.slice(11,19);
     chat.innerHTML += `
@@ -15,7 +38,3 @@ function processDateResponse(time) {
     `;
     return time;}
     
-function processError(error) {
-    console.log("Status Code: " + error.response.status); // Ex: 404
-    console.log("Error Message: " + error.response.data); // Ex: Not Found
-}
